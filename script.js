278b28d9bcfd5065c3ffa62cc431b8996f7fdf9c
Azhwake <azhwaq2002@gmail.com>
2024-09-21 22:16:32 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Good Luck 💪🏾
+function secondLargest(arr) {
+    let sortedArr = arr.sort((a, b) => b - a);  
+    return sortedArr[1]; 
+}
+
+
+console.log(secondLargest([23, 66, 50, 12, 6]));  // ➞ 50
+console.log(secondLargest([65, 34, 100, 22, 85]));  // ➞ 85
+console.log(secondLargest([67, 22, 19, 12, 80]));  // ➞ 67
